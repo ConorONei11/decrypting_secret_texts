d6b773d37f8be7c961724fef24e4b6a233b76478
ConorONei11 <124355679+ConorONei11@users.noreply.github.com>
2023-02-10 15:56:02 +0000

    Update Extended euclidean algorithm.py

--- Extended euclidean algorithm.py
+++ Extended euclidean algorithm.py
@@ -5,9 +5,6 @@ Created on Thu Feb 17 12:31:34 2022
 @author: Con67
 """
 #Extended Euclidean algorithm
-a=1632
-b=642
- 
 #Finding gcd of two intgers a and b
 def gcd(a,b):
     while b>0:
@@ -16,9 +13,11 @@ def gcd(a,b):
       b=r
     return(a)
 
-print(gcd(1632,642))
+#Finding p and q
 print(gcd(51525,99157))
 print(gcd(51527,99157))
+#Therefore p = 229 and q = 433 (correct since both prime)
+#Now transmitted can begin to be encrypted ... ->
 
 #Finding gcd of two integers a and b
 #Also finding n and m where n*a+m*b = gcd(a,b)
@@ -41,5 +40,7 @@ def gcd2(a,b):
         y0=y00
     return(a,x1,y1)
 
-print(gcd2(1288,17))
+#Since (p-1)(q-1)=98496 and public key e = 289
 print(gcd2(98496,289))
+
+#Find that the private key d = 20449, and the message can now be decrypted (as seen on decrypted dataset file)
