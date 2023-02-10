c05a35e71d270308e364ed7c0fa250cccf73edab
ConorONei11 <124355679+ConorONei11@users.noreply.github.com>
2023-02-10 15:45:44 +0000

    Update Factorization.py

--- Factorization.py
+++ Factorization.py
@@ -6,7 +6,9 @@ Created on Fri Feb 18 10:49:00 2022
 """
 
 #Factorisation
-#Choose a=281 since this is prime and gcd(281,99157)=1
+#We want to find p and q such that pq = 99157
+#Before we can do this we need to calculate the successive powers of 281 mod 99157
+#a=281 since this is prime and gcd(281,99157)=1
 #Factorisation to find period r and x, where gcd(x+-1,n) = p and q
 i=1
 while True:
@@ -19,4 +21,4 @@ while True:
          print(x)
          break
 
-
+#The result is that r = 8208 and x = 51526
